09e9c6c61c73b2a5fd65b3e190f4339882f7d123
Jaypeegee0 <okojijohnpaul0@gmail.com>
2019-09-19 13:24:42 +0100

    changes and edit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
 
    $.get("http://localhost:3000/animals", function(data){
-         for(var i =0; i<data.length;i++){
-            var tab= '<tr><td>' + data[i].name + '</td>';
-            tab +='<td>' + data[i].specie + '</td>';
-            tab +='<td>' + data[i].feeding + '</td>';
-            tab +='<td>' + data[i].covering + '</td>';
-            tab +='<td>' + "<button type ='button' class ='btn btn-info' id='editAnimal'><a href= 'form1.html'>View</a></button>"  + '</td>';
-            tab +='<td>' + "<button type ='button' class ='btn btn-danger' id = 'deleteAnimal'>Delete</button>"  + '</td></tr>';
-            tab+= "<p>The " + data[i].name + "is a/an" + data[i].specie + '.</p>';
-            $("table tbody").append(tab);}
+         for(var i =0; i<data.length;i++){$("table tbody").append(`<tr><td>${data[i].id}</td>
+         <td>${data[i].data}</td>
+         <td>${data[i].specie}</td>
+         <td>${data[i].feeding}</td>
+         <td>${data[i].covering}</td>
+         <td><button type ='button' class ='btn btn-info' id='editAnimal'><a href= 'form1.html?id=${data[i].id}'>View</a></button></td>
+         <td><button type ='button' class ='btn btn-danger' id = 'deleteAnimal'>Delete</button></td></tr>
+         `);}
+        //     var tab= '<tr><td>' + data[i].name + '</td>';
+        //     tab +='<td>' + data[i].specie + '</td>';
+        //     tab +='<td>' + data[i].feeding + '</td>';
+        //     tab +='<td>' + data[i].covering + '</td>';
+        //     tab +='<td>' + "<button type ='button' class ='btn btn-info' id='editAnimal'><a href= 'edit.html?id=${}'>View</a></button>"  + '</td>';
+        //     tab +='<td>' + "<button type ='button' class ='btn btn-danger' id = 'deleteAnimal'>Delete</button>"  + '</td></tr>';
+        //     tab+= "<p>The " + data[i].name + "is a/an" + data[i].specie + '.</p>';
+        //     $("table tbody").append(tab);}
         });
 
-     $("#addAnimal").submit(function(event){
+     $("#addAnimalssss").submit(function(event){
         event.preventDefault();
         var nameVal= ("#animalName").val();
         var specieVal= ("#specie").val();
