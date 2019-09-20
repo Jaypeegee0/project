bf81ccd0d6e90ba58771ce5b824e56823295cf66
Jaypeegee0 <okojijohnpaul0@gmail.com>
2019-09-20 09:35:44 +0100

    done

---
+++ public/README.md
@@ -0,0 +1,8 @@
+This is a simple zoo website that displays animals in a specified zoo the performs the following functions
+
+- A user can view all animals
+- A user can add an animal that has been identified in the zoo
+- A user can delete a selected animal
+- A user can view a selected animal
+
+NB: A lot of debugging and functionalities are yet to be added.
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
     $.get("http://localhost:3000/animals", function(data){
-        for(var i =0; i<data.length;i++){$("table tbody").append(`<tr><td>${data[i].id}</td>
-        <td>${data[i].name}</td>
+        for(var i =0; i<data.length;i++){$("table tbody").append(`<tr><td>${data[i].name}</td>
         <td>${data[i].specie}</td>
         <td>${data[i].feeding}</td>
         <td>${data[i].covering}</td>
-        <td><button type ='button' class ='view btn btn-info' id="'${data[i].id}'"><a href = 'form1.html?id=${data[i].id}'>View</a></button></td></tr>`);}
+        <td><button type ='button' class ='view btn btn-info' id="'${data[i].id}'"><a href = 'form1.html?id=${data[i].id}'>View</a></button></td> 
+        <td><button type ='button' class ='deleteAnimal btn btn-danger' value=${data[i].id}>Delete</button></td></tr>`);}
 
-    if(path === "form1.html"){
+    if(path === "/form1.html"){
         const url= window.location.href;
         const urlArray = url.split("id=");
         let id = urlArray[1];
@@ -21,45 +21,22 @@ $(document).ready(function(){
         $("#specieDetail").html(data.specie);
         $("#feedingDetail").html(data.feeding);
         $("#coveringDetail").html(data.covering);
+        $("#view").html('<a href= "form1.html?id='+data.id+'" class= "btn btn-warning" id = "update-button">UPDATE</a>');
      });
     }
 
-    $("#deleteAnimal").click( function (e) { 
+    $(".deleteAnimal").click( function (e) { 
         e.preventDefault();
         const id = $(this).val();
         $.ajax({
             url: `http://localhost:3000/animals/${id}`,
             method: "DELETE",
             success: function (result) {
-                alert("Deleted");
+                alert("Deleted! Please refresh page!!");
               }
         });
      });
 
-
-    
-    //      $(".view").click((e)=>{
-    //         e.preventDefault();
-    //         $('.viewer').toggle();
-    //         console.log(e.target.id);
-
-    //  $.get(`http://localhost:3000/animals`,(data)=>{
-    //     $('.viewer').html('');
-    //             for(let i=0; i<data.length;i++){
-    //                 $('.viewer').append('<p>'+data[i].name+' is a great '+data[i].covering+'</p>');
-    //                 }
-
-    //           });
-    
-    //     });
-        //     var tab= '<tr><td>' + data[i].name + '</td>';
-        //     tab +='<td>' + data[i].specie + '</td>';
-        //     tab +='<td>' + data[i].feeding + '</td>';
-        //     tab +='<td>' + data[i].covering + '</td>';
-        //     tab +='<td>' + "<button type ='button' class ='btn btn-info' id='editAnimal'><a href= 'edit.html?id=${}'>View</a></button>"  + '</td>';
-        //     tab +='<td>' + "<button type ='button' class ='btn btn-danger' id = 'deleteAnimal'>Delete</button>"  + '</td></tr>';
-        //     tab+= "<p>The " + data[i].name + "is a/an" + data[i].specie + '.</p>';
-        //     $("table tbody").append(tab);}
         });
 
      $("#addAnimal").submit(function(event){
@@ -78,12 +55,4 @@ $(document).ready(function(){
     });
 
     var path = $(location).attr('pathname');
-    
-    // if ( path == '/form1.html'){
-    //     const url = window.location.href;
-    //     const urlArray = url.split("id");
-    //     let id = urlArray[i];
-    //     id = parseInt(id);
-    // }
-
 });
